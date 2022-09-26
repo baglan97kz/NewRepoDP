dcb63e3eb3ca03a963a68a2176e814888c996278
Zhandarbek Krutoi <ospansuper@gmail.com>
2022-09-26 17:35:19 +0600

    Tortinshi

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
+using System;
+// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+
+
+class Mail {
+    
+
+}
